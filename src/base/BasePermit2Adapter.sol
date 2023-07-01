bd882914b7572a20c77d1968c1a71d219c840ec7
Sam Bugs <sambugs@protonmail.com>
2023-07-01 11:14:08 -0300

    fix: lint

--- src/base/BasePermit2Adapter.sol
+++ src/base/BasePermit2Adapter.sol
@@ -13,12 +13,14 @@ abstract contract BasePermit2Adapter is IBasePermit2Adapter {
   /// @inheritdoc IBasePermit2Adapter
   address public constant NATIVE_TOKEN = Token.NATIVE_TOKEN;
   /// @inheritdoc IBasePermit2Adapter
+  // solhint-disable-next-line var-name-mixedcase
   IPermit2 public immutable PERMIT2;
 
   constructor(IPermit2 _permit2) {
     PERMIT2 = _permit2;
   }
 
+  // solhint-disable-next-line no-empty-blocks
   receive() external payable { }
 
   modifier checkDeadline(uint256 _deadline) {
