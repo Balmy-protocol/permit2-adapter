2a3b5a2ef6d62a399bd87d3240656f4c353a3be6
Sam Bugs <sambugs@protonmail.com>
2023-07-01 12:22:36 -0300

    feat: add swap adapter

---
+++ test/unit/instances/SwapPermit2AdapterInstance.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.0;
+
+import { SwapPermit2Adapter } from "../../../src/base/SwapPermit2Adapter.sol";
+import { BasePermit2Adapter, IPermit2 } from "../../../src/base/BasePermit2Adapter.sol";
+
+contract SwapPermit2AdapterInstance is SwapPermit2Adapter {
+  constructor(IPermit2 _permit2) BasePermit2Adapter(_permit2) { }
+}
