954ce732c3e86ca676ac33c41739b9b37d8e4c82
Sam Bugs <sambugs@protonmail.com>
2023-07-18 09:40:37 -0300

    chore: fix type and use zero salt for deployments

--- script/DeployUniversalAdapter.s.sol
+++ script/DeployUniversalAdapter.s.sol
@@ -5,12 +5,11 @@ import { console2 } from "forge-std/console2.sol";
 import { UniversalPermit2Adapter, IPermit2 } from "../src/UniversalPermit2Adapter.sol";
 import { BaseScript } from "./Base.s.sol";
 
-bytes32 constant SALT = bytes32(uint256(3_377_442));
 IPermit2 constant PERMIT2 = IPermit2(0x000000000022D473030F116dDEE9F6B43aC78BA3);
 
-contract DeployUnversalAdapter is BaseScript {
+contract DeployUniversalAdapter is BaseScript {
   function run() public broadcaster returns (UniversalPermit2Adapter _adapter) {
-    _adapter = new UniversalPermit2Adapter{salt: SALT}(PERMIT2);
+    _adapter = new UniversalPermit2Adapter{salt: ZERO_SALT}(PERMIT2);
     console2.log("Permit2 Deployed:", address(_adapter));
   }
 }
