ef6b5fc22be1be0e97e23cd8d2f68718209f3c6b
Sam Bugs <sambugs@protonmail.com>
2023-07-31 15:10:42 -0300

    refactor: add simulation adapter

--- src/interfaces/ISwapPermit2Adapter.sol
+++ src/interfaces/ISwapPermit2Adapter.sol
@@ -69,21 +69,6 @@ interface ISwapPermit2Adapter is IBasePermit2Adapter {
     returns (uint256 amountIn, uint256 amountOut);
 
   /**
-   * @notice Executes a sell order swap by proxing to another contract, but using Permit2 to transfer tokens from the
-   * caller
-   * @dev Not meant to be used on-chain! The idea behind this function is to have a way to simulate a swap and get
-   *      amount in spent, the amount out received, and the gas spent on the swap. All in one RPC call
-   * @param params The swap's data, such as tokens, amounts, recipient, etc
-   * @return amountIn The amount ot `token in` spent on the swap
-   * @return amountOut The amount of `token out` produced by the proxied swap
-   * @return gasSpent The gas spent on the entire swap
-   */
-  function sellOrderSwapWithGasMeasurement(SellOrderSwapParams calldata params)
-    external
-    payable
-    returns (uint256 amountIn, uint256 amountOut, uint256 gasSpent);
-
-  /**
    * @notice Executes a buy order swap by proxing to another contract, but using Permit2 to transfer tokens from the
    * caller
    * @param params The swap's data, such as tokens, amounts, recipient, etc
@@ -95,18 +80,4 @@ interface ISwapPermit2Adapter is IBasePermit2Adapter {
     payable
     returns (uint256 amountIn, uint256 amountOut);
 
-  /**
-   * @notice Executes a buy order swap by proxing to another contract, but using Permit2 to transfer tokens from the
-   * caller
-   * @dev Not meant to be used on-chain! The idea behind this function is to have a way to simulate a swap and get
-   *      amount in spent, the amount out received, and the gas spent on the swap. All in one RPC call
-   * @param params The swap's data, such as tokens, amounts, recipient, etc
-   * @return amountIn The amount ot `token in` spent on the swap
-   * @return amountOut The amount of `token out` produced by the proxied swap
-   * @return gasSpent The gas spent on the entire swap
-   */
-  function buyOrderSwapWithGasMeasurement(BuyOrderSwapParams calldata params)
-    external
-    payable
-    returns (uint256 amountIn, uint256 amountOut, uint256 gasSpent);
 }
--- test/integration/SwapPermit2Adapter.t.sol
+++ test/integration/SwapPermit2Adapter.t.sol
@@ -50,7 +50,7 @@ contract SwapPermit2AdapterTest is PRBTest, StdCheats {
 
     // Execute
     vm.prank(alice);
-    (uint256 _returnedAmountIn, uint256 _returnedAmountOut,) = adapter.sellOrderSwapWithGasMeasurement(
+    (uint256 _returnedAmountIn, uint256 _returnedAmountOut) = adapter.sellOrderSwap(
       ISwapPermit2Adapter.SellOrderSwapParams({
         deadline: DEADLINE,
         tokenIn: address(USDC),
@@ -90,7 +90,7 @@ contract SwapPermit2AdapterTest is PRBTest, StdCheats {
 
     // Execute
     vm.prank(alice);
-    (uint256 _returnedAmountIn, uint256 _returnedAmountOut,) = adapter.sellOrderSwapWithGasMeasurement{
+    (uint256 _returnedAmountIn, uint256 _returnedAmountOut) = adapter.sellOrderSwap{
       value: _amountToSwap
     }(
       ISwapPermit2Adapter.SellOrderSwapParams({
@@ -135,7 +135,7 @@ contract SwapPermit2AdapterTest is PRBTest, StdCheats {
 
     // Execute
     vm.prank(alice);
-    (uint256 _returnedAmountIn, uint256 _returnedAmountOut,) = adapter.buyOrderSwapWithGasMeasurement(
+    (uint256 _returnedAmountIn, uint256 _returnedAmountOut) = adapter.buyOrderSwap(
       ISwapPermit2Adapter.BuyOrderSwapParams({
         deadline: DEADLINE,
         tokenIn: address(DAI),
