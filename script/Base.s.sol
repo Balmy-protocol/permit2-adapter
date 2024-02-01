46fbb6860a3383cbaf35be1ca383e67b6efce644
Sam Bugs <sambugs@protonmail.com>
2024-02-01 11:01:06 -0300

    fix: lint

--- script/Base.s.sol
+++ script/Base.s.sol
@@ -4,12 +4,10 @@ pragma solidity >=0.8.19;
 import { Script } from "forge-std/Script.sol";
 
 abstract contract BaseScript is Script {
-
   /// @dev Needed for the deterministic deployments.
   bytes32 internal constant ZERO_SALT = bytes32(0);
 
-  constructor() {
-  }
+  constructor() { }
 
   modifier broadcaster() {
     vm.startBroadcast();
